5f0366ce8e3babc0fba2f951aebb5f8044a8dfa7
Emily Creamer <emily.creamer@icloud.com>
2019-04-11 10:28:34 -0700

    page 30

--- router.js
+++ router.js
@@ -1,7 +1,13 @@
-function route(pathname) {
-  console.log("About to route a request for" + pathname);
+function route(handle, pathname, response) {
+  console.log("About to route a request for " + pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](response);
+  } else {
+    console.log("No request handler found for " + pathname);
+    response.writeHead(404, {"Content-type": "text/plain"});
+    response.write("404: Not found.");
+    response.end();
+  }
 }
 
 exports.route = route;
-
-//page 18
