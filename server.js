0bcdf96e84c37c6d70d185771cf82b0f4ffd19d9
Emily Creamer <emily.creamer@icloud.com>
2019-04-11 08:24:42 -0700

    page 18

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const http = require("http");
+var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text.plain"});
-  response.write("Hello, world.");
-  response.end();
-}).listen(8888);
+function start() {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for" + pathname + " received.");
+    response.writeHead(200, {"Content-Type": "text.plain"});
+    response.write("Hello, world.");
+    response.end();
+  }
 
-// http.createServer(onRequest)).listen(8888);
-console.log("Server has started.");
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
